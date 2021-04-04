fa2f92245637bc947e947e82751a6e7f3e3d8eb8
kerenin95 <weberbencs@gmail.com>
2021-04-04 01:28:29 -0400

    commit

---
+++ NumberGuessingGame/GuessingGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class GuessingGame
+    {
+        static int secretNumber()
+        {
+            Random r = new Random();
+            int secretNum = r.Next(101);
+
+            return secretNum;
+        }
+        static void Main(string[] args)
+        {
+            int tries = 0;
+            int sN = secretNumber();
+
+            Console.WriteLine("Please Guess a number between 1 and 100:\n");                       
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            while (guess != sN)
+            {
+                if (guess > sN)
+                {
+                    Console.WriteLine($"You are too high \nPlease guess again:\n");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (guess < sN)
+                {
+                    Console.WriteLine($"You are too low \nPlease guess again:\n");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+                tries++;
+            }
+
+            Console.WriteLine($"Congratulations, you guessed the number in {tries} tries!");
+
+            Console.ReadLine();
+        }
+    }
+}
